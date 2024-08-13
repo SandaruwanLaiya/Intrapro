645e5ff8b375accefddb59b9f950d394345e3a03
Lahiru Sandaruwan <l.sandaruwan@hayleysadvantis.com>
2024-08-13 16:23:42 +0530

    layout changes 2024-08-13

--- Intrapro/Intrapro/Content/js/MainLayout.js
+++ Intrapro/Intrapro/Content/js/MainLayout.js
@@ -1,1 +1,22 @@
-﻿
+﻿document.getElementById('userIcon').addEventListener('click', function () {
+    var panel = document.getElementById('userDetailsPanel');
+    if (panel.style.display === "none" || panel.style.display === "") {
+        panel.style.display = "block";
+    } else {
+        panel.style.display = "none";
+    }
+});
+
+function LogOut() {
+    window.location.href = '/Administrator/Index';
+    //$.ajax({
+    //    url: '/Home/Index',
+    //    type: 'GET', // or 'POST' if the Index method requires a POST request
+    //    success: function (response) {
+    //        alert("Success: " + response);
+    //    },
+    //    error: function (error) {
+    //        alert("Error: " + error);
+    //    }
+    //});
+}
